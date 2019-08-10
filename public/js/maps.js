3e115eb9c16f496ab99e424aae5dedfb73591157
huynguyenau <huynguyenau@gmail.com>
2019-08-10 12:18:35 +1000

    Temp fix maps

--- public/js/maps.js
+++ public/js/maps.js
@@ -1,6 +1,7 @@
-$(document).ready(function () {
+$(document).ready(function() {
     // Azure Maps.
-    var ready = false, user_position_marker, user_position = [144.96292, -37.80737],
+    var ready = false,
+        user_position_marker, user_position = [144.96292, -37.80737],
         map = new atlas.Map('LoadMap', {
             center: user_position,
             authOptions: {
@@ -8,26 +9,27 @@ $(document).ready(function () {
                 subscriptionKey: 'Ax6CHWnkkH7Zjt1uoQvH8TfBspFTMkPPybuLWF0V8_M'
             },
             enableAccessibility: true,
-        }), controls = [];
+        }),
+        controls = [];
 
     var webSocket = new WebSocket('wss://' + location.host);
-    webSocket.onopen = function () {
+    webSocket.onopen = function() {
         console.log('Successfully connect WebSocket');
     }
 
-    webSocket.onmessage = function (message) {
+    webSocket.onmessage = function(message) {
         console.log('Received message: ' + message.data);
         try {
             var obj = JSON.parse(message.data);
             const maxLen = 100;
 
             // Make sure the MQTT message contains all of the following fields.
-            // Else, it's not what we want.
-            if (!obj.time || !obj.temp || !obj.humidity || !obj.o2 || !obj.co2 || !obj.accel
-                || !obj.shelf_life || !obj.ethylene || !obj.lon || !obj.lat) {
-                console.log('Message contains unexpected contents: ' + obj);
-                return;
-            }
+            // // Else, it's not what we want.
+            // if (!obj.time || !obj.temp || !obj.humidity || !obj.o2 || !obj.co2 || !obj.accel
+            //     || !obj.shelf_life || !obj.ethylene || !obj.lon || !obj.lat) {
+            //     console.log('Message contains unexpected contents: ' + obj);
+            //     return;
+            // }
 
             // Location.
             document.getElementById("location").innerHTML = "Lon: " + obj.lon + ", Lat: " + obj.lat;
@@ -77,7 +79,7 @@ $(document).ready(function () {
         });
     }
 
-    map.events.add('ready', function () {
+    map.events.add('ready', function() {
         //Add controls to the map.
         map.controls.add(
             new BringDataIntoViewControl({
