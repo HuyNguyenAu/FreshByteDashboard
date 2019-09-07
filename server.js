26602d7da33127705db955a86dfb1b435a6a487b
huynguyenau <huynguyenau@gmail.com>
2019-09-07 16:38:57 +1000

    asd

--- server.js
+++ server.js
@@ -74,4 +74,6 @@ function normalizePort(val) {
 //     }
 // }
 
-wss.broadcast(JSON.stringify(process.env['Azure.SQL.Database.Test']));
+setTimeout(function() {
+    wss.broadcast(JSON.stringify(process.env['Azure.SQL.Database.Test']));
+}, 5000);
