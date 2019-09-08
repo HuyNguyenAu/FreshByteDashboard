48bfeec5eeccbcac21cf182aba0b743314d13717
huynguyenau <huynguyenau@gmail.com>
2019-09-08 20:52:38 +1000

    DA updated

--- public/js/data_analytics.js
+++ public/js/data_analytics.js
@@ -93,64 +93,16 @@ $(document).ready(function() {
     var webSocket = new WebSocket('wss://' + location.host + '/');
     webSocket.onopen = function() {
         console.log('Successfully connect WebSocket');
-        // Get maps subscription key.
-        webSocket.send("maps");
         // Call server to send SQL data.
         // webSocket.send("sql");
     }
 
-    // Azure Maps.
-    var ready = false,
-        user_position_marker,
-        user_position = [144.96292, -37.80737],
-        map,
-        controls = [];
     // Update the data arrays and dashboard elements to latest MQTT message received.
     // Keep the code complexity out of the dashboard.
     // The dashboard only displays the information received, arduino does the pre-processing.
     // !!! Need a better way to handle the repeated code blocks below.
     webSocket.onmessage = function(message) {
-        // Setup maps when key received.
-        if (message.data.includes('Azure.Maps.SubscriptionKey ')) {
-            map = new atlas.Map('map', {
-                center: user_position,
-                authOptions: {
-                    authType: 'subscriptionKey',
-                    subscriptionKey: message.data.replace(/Azure.Maps.SubscriptionKey\s/, "").replace(/"/g, '')
-                },
-                enableAccessibility: true,
-            });
-
-            function addControls() {
-                map.controls.remove(controls);
-                controls = [];
-                var controlStyle = "light";
-                // Zoom.
-                controls.push(new atlas.control.ZoomControl({
-                    zoomDelta: 1,
-                    style: controlStyle
-                }));
-                map.controls.add(controls, {
-                    position: "top-right"
-                });
-            }
-
-            map.events.add('ready', function() {
-                //Add controls to the map.
-                map.controls.add(
-                    new BringDataIntoViewControl({
-                        units: 'metric'
-                    }), {
-                        position: 'top-left'
-                    });
-
-                ready = true;
-            });
-            map.events.add('ready', addControls);
-
-        } else {
-            console.log('Received message: ' + message.data);
-        }
+        console.log('Received message: ' + message.data);
 
         try {
             var obj = JSON.parse(message.data);
