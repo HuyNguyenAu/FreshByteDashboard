e5d49bc7f6bd5188557f533703d63d1c7c3e9d5f
huynguyenau <huynguyenau@gmail.com>
2019-09-09 19:16:47 +1000

    a

--- public/js/data_analytics.js
+++ public/js/data_analytics.js
@@ -106,9 +106,9 @@ function GetIDs() {
     tr = table.getElementsByTagName("tr");
     // Loop through all table rows, and hide those who don't match the search query
     for (i = 0; i < tr.length; i++) {
-        console.log(tr.length);
         if (tr[i].style.display == "") {
             ids.push(tr[i].getElementsByTagName("td")[0].innerText);
+            console.log(i);
         }
     }
 
