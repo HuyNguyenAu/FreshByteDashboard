2484d66a0f6d141a94d22676f6449d8fe778d741
huynguyenau <huynguyenau@gmail.com>
2019-09-09 21:33:10 +1000

    Final Test

--- public/js/data_analytics.js
+++ public/js/data_analytics.js
@@ -252,6 +252,14 @@ function DefaultCharts() {
 $(document).ready(function() {
     var count = 0;
 
+    temp_chart = chart("temp-chart", "Temperature (Celsius)", time_data, temp_data, "rgba(255, 99, 132, 1)", "rgba(255, 99, 132, 0.4)");
+    humidity_chart = chart("humidity-chart", "Humidity (%)", time_data, humidity_data, "rgba(54, 162, 235, 1)", "rgba(54, 162, 235, 0.4)");
+    o2_chart = chart("o2-chart", "O2 (%)", time_data, o2_data, "rgba(75, 192, 192, 1)", "rgba(75, 192, 192, 0.4)");
+    co2_chart = chart("co2-chart", "CO2 (%)", time_data, co2_data, "rgba(145, 97, 242, 1)", "rgba(145, 97, 242, 0.4)");
+    accel_chart = chart("accel-chart", "Accel |m/s^2|", time_data, accel_data, "rgba(255, 205, 86, 1)", "rgba(255, 205, 86, 0.4)");
+    shelf_life_chart = chart("shelf-life-chart", "Shelf Life (Days)", time_data, shelf_life_data, "rgba(255, 201, 14, 1)", "rgba(255, 201, 14, 0.4)");
+    ethylene_chart = chart("ethylene-chart", "Ethyene pmol/(kgs)", time_data, ethylene_data, "rgba(128, 64, 64, 1)", "rgba(128, 64, 64, 0.4)");
+
     var webSocket = new WebSocket('wss://' + location.host + '/');
     webSocket.onopen = function() {
         console.log('Successfully connect WebSocket');
@@ -345,8 +353,35 @@ $(document).ready(function() {
                 ethylene_data.shift();
             }
 
-            // Update charts.
-            DefaultCharts();
+            document.getElementById("temp").textContent = Average(temp_data) + "°C";
+            UpdateMaxMin(temp_data, "temp-max", "temp-min", "°C");
+
+            document.getElementById("humidity").textContent = Average(humidity_data) + "%";
+            UpdateMaxMin(humidity_data, "humidity-max", "humidity-min", "%");
+
+            document.getElementById("o2").textContent = Average(o2_data) + "%";
+            UpdateMaxMin(o2_data, "o2-max", "o2-min", "%");
+
+            document.getElementById("co2").textContent = Average(co2_data) + " ppm";
+            UpdateMaxMin(co2_data, "co2-max", "co2-min", "ppm");
+
+            document.getElementById("accel").textContent = Average(accel_data) + " |m/s^2|";
+            UpdateMaxMin(accel_data, "accel-max", "accel-min", "|m/s^2|");
+
+            document.getElementById("shelf-life").textContent = Average(shelf_life_data) + ' days';
+            UpdateMaxMin(shelf_life_data, "shelf-life-max", "shelf-life-min", "days");
+
+            UpdateMaxMin(ethylene_data, "ethylene-max", "ethylene-min", "pmol/(kg*s)");
+            document.getElementById("ethylene").textContent = Average(ethylene_data) + ' pmol/(kg*s)';
+
+            // Update the charts to reflect data array changes.
+            temp_chart.update();
+            humidity_chart.update();
+            o2_chart.update();
+            co2_chart.update();
+            accel_chart.update();
+            shelf_life_chart.update();
+            ethylene_chart.update();
 
             // Update table.
             var row = document.getElementById("table").insertRow(-1);
